f535c3833e9b31ffc7d26e2cbfb2029702853eab
longrise <longrise@longrisedeMBP.lan>
2018-12-19 17:06:41 +0800

    1

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,20 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by longrise on 2018/12/19.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
+
+NS_ASSUME_NONNULL_END
