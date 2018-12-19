c0fadf0a2c61595f922f66eaca955870eb893f71
潘神 <415391360@qq.com>
2018-12-19 16:26:58 +0800

    Initial Commit

---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by longrise on 2018/12/19.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ B_Category/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  B_Category
+//
+//  Created by longrise on 2018/12/19.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
